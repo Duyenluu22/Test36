77312d2a259946de1e1ffa4c1d07c10ad9a5a015
Duyenluu22 <duyenluu22041997@gmail.com>
2026-06-03 06:08:41 +0700

    test 36

---
+++ main.js
@@ -0,0 +1,17 @@
+// Hàm căn bậc 2
+let a = Math.sqrt(9);
+console.log(a);
+
+let b = Math.sqrt(25);
+console.log(b);
+//Hàm luỹ thừa
+let c = 2;
+let d = 3;
+let kq = Math.pow(c, d);
+console.log(kq);
+let luyThua = Math.pow(2, 4);
+console.log(luyThua);
+
+//Giá trị tuyệt đối
+let tuyetDoi = Math.abs(-10);
+console.log(tuyetDoi);
